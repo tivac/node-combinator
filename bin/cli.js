8cfb531da1e2f1b82433b26f00837a5a83de5cc8
Pat Cavit <pcavit@gmail.com>
2012-11-29 12:59:14 -0800

    (Mostly) fully armed & operational
    File saving is in, though somewhat wonky
    Re-jiggered CLI args a bunch
    Totally re-worked how the cli executes
    Better checks for the types of elements we care about
    Filtering based on the src/href of the elements (so you can combo up the
    right modules vs everyone)

--- bin/cli.js
+++ bin/cli.js
@@ -3,26 +3,58 @@
 
 "use strict";
 
-var fs   = require("fs"),
-    path = require("path"),
-    argv = require("optimist")
-            .usage("Update <script> tags to use combo URLs.\nUsage: $0 -r .")
-            .options(require("./args.json"))
-            .argv,
-
+var fs         = require("fs"),
+    util       = require("util"),
+    path       = require("path"),
+    mkdirp     = require('mkdirp'),
     Combinator = require("../lib/combinator.js"),
-    combinator = new Combinator(argv),
-    output     = combinator.run(),
-    _save;
+    optimist   = require("optimist")
+        .usage("\nCombine multiple <script> & <link> tags into single combo-handled tags.\nUsage: $0 -r .")
+        .options(require("./args.json")),
+    
+    _argv = optimist.argv;
 
-_save = function(output) {
-    var file = path.resolve(argv.output);
-
-    fs.writeFileSync(file, output);
-};
-
-if(argv.output) {
-    _save(output);
-} else {
-    console.log(output);
+if(_argv.help) {
+    return optimist.showHelp();
 }
+
+(new Combinator(_argv)).run(function(error, results) {
+    var output;
+    
+    if(error) {
+        return console.error(util.inspect(error, null, null, true));
+    }
+    
+    if(!_argv.output) {
+        return console.log(util.inspect(results, null, null, true));
+    }
+    
+    output = path.resolve(_argv.output);
+    
+    mkdirp.sync(output);
+    
+    results.forEach(function(details) {
+        var file = path.join(output, details.file),
+            ext  = path.extname(file),
+            dir;
+        
+        if(!details.text) {
+            return;
+        }
+        
+        file = file.replace(ext, _argv.suffix + ext);
+        
+        mkdirp.sync(path.dirname(file));
+        
+        fs.writeFile(file, details.text, function(err) {
+            if(err) {
+                console.error("Unable to write " + file);
+                console.error(err);
+            }
+        });
+    });
+});
+
+
+
+
